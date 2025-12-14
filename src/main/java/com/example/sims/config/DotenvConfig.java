d2c345c54149a9d678518fb940b72a09585a62a5
GitheTrunk <bsengleang@gmail.com>
2025-12-14 16:56:20 +0700

    Initial commit and setup some dependencies

---
+++ src/main/java/com/example/sims/config/DotenvConfig.java
@@ -0,0 +1,18 @@
+package com.example.sims.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DotenvConfig {
+
+    public DotenvConfig() {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+        
+        dotenv.entries().forEach(entry -> 
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
+    }
+}
